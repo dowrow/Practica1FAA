e62355c8c3893d7facc5d687b307bca6f33ca9eb
eps <eps@150.244.65.31>
2014-09-19 20:03:15 +0200

    OJITO!

--- Practica1FAA/src/datos/Datos.java
+++ Practica1FAA/src/datos/Datos.java
@@ -5,21 +5,27 @@
  */
 package datos;
 
+import com.csvreader.CsvReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import particionado.Particion;
 
 public class Datos {
 
     public enum TiposDeAtributos {
-
         Continuo, Nominal
     };
 
     ArrayList<TiposDeAtributos> tipoAtributos;
-
-    double[][] datos;
-
+    ArrayList<String> nombreCampos;
+    Elemento datos[][];
+    
     public Datos(int numDatos, ArrayList<TiposDeAtributos> tipos) {
+        this.tipoAtributos = tipos;
     }
 
     public Datos extraeDatosTrain(Particion idx) {
@@ -29,8 +35,39 @@ public class Datos {
     public Datos extraeDatosTest(Particion idx) {
         return null;
     }
-
+    
+    // 3 Filas: nº datos, nombres de campos, tipos de atributos [Nominal o Continuo]
+    // Resto de filas: Conjunto de datos, uno por fila y campos separados por comas
     public static Datos cargaDeFichero(String nombreDeFichero) {
+        CsvReader ficheroCSV;
+        ArrayList<String> nombreAtributos;
+        
+        try {
+            ficheroCSV = new CsvReader(nombreDeFichero);
+            
+            // Lee nº datos
+            ficheroCSV.readRecord();
+            int numFilas = Integer.parseInt(ficheroCSV.get(0));
+                        
+            // Lee nombres campos
+            ficheroCSV.readRecord();
+            nombreAtributos = Arrays.asList(ficheroCSV.getValues());
+                    
+            // Lee tipos de atributos
+            while (ficheroCSV.readRecord()) {
+               
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Datos.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Datos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+      
+        
+       
+         
+        usuarios_import.close();
         return null;
     }
 }
