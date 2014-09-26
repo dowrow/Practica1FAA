e427718809306c8ee6f5264d32884c8228a811e6
eps <eps@150.244.65.48>
2014-09-26 19:20:23 +0200

    Implementada Division porcentual

--- Practica1FAA/src/particionado/DivisionPorcentual.java
+++ Practica1FAA/src/particionado/DivisionPorcentual.java
@@ -6,9 +6,13 @@
 package particionado;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 
 public class DivisionPorcentual implements EstrategiaParticionado {
-
+    
+    private final double porcentajeTrain = 0.6;
+    
     @Override
 // Devuelve el nombre de la estrategia de particionado
     public String getNombreEstrategiaParticionado() {
@@ -16,9 +20,27 @@ public class DivisionPorcentual implements EstrategiaParticionado {
     }
 
     @Override
-// Crea particiones segun el metodo tradicional. El conjunto de entrenamiento
+    // Crea particiones segun el metodo tradicional. El conjunto de entrenamiento
     // se crea con un porcentaje y el de test con el restante
     public ArrayList<Particion> crearParticiones(int numDatos, int numParticiones) {
-        return null;
+        
+        int numDatosTrain = (int) (numDatos * this.porcentajeTrain);
+        
+        // Todos los indices
+        ArrayList<Integer> indices = new ArrayList<>();
+        for (int i = 0; i < numDatos; i++) {
+            indices.add(i);
+        }
+        
+        ArrayList<Particion> particiones = new ArrayList<>();
+        for (int i = 0; i < numParticiones; i++) {
+            Collections.shuffle(indices);
+            ArrayList<Integer> indicesTrain = new ArrayList<>(indices.subList(0, numDatosTrain - 1));
+            ArrayList<Integer> indicesTest  = new ArrayList<>(indices.subList(numDatosTrain, numDatos - 1));
+            Particion p = new Particion(indicesTrain, indicesTest);
+            particiones.add(p);        
+        }       
+        
+        return particiones;
     }
 }
