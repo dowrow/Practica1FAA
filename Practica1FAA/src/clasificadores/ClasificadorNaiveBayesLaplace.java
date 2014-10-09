4f48dbef924a03a5ba00aed96a72b7383b5dae7d
dani <dani@dani-PC>
2014-10-09 16:42:54 +0200

    arreglados unos conteos de números creo

--- Practica1FAA/src/clasificadores/ClasificadorNaiveBayesLaplace.java
+++ Practica1FAA/src/clasificadores/ClasificadorNaiveBayesLaplace.java
@@ -150,15 +150,6 @@ public class ClasificadorNaiveBayesLaplace extends Clasificador{
                         contador++;
                         incidenciaClase.put(clase, contador);
                         incidenciaColumna.put(fila[i], incidenciaClase);
-
-                        //para hacer laplace, cada vez que generas la tabla de unos
-                        //sumas uno a las incidencias de clases totales
-                        for(Elemento claseAux : datosTrain.getClases()){
-                            Integer nIncidencias = this.incidenciaClaseTotal.get(claseAux);
-                            nIncidencias++;
-                            this.incidenciaClaseTotal.put(claseAux, nIncidencias);
-                            this.filasTrain++;
-                        }
                     }
                 }
                 
